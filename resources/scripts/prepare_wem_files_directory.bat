1d336d7da823633865431f14f0f29fd3cf7ad089
BuildTools <javgarag2@gmail.com>
2026-04-10 22:41:55 +0200

    learn to comment a batch script

--- resources/scripts/prepare_wem_files_directory.bat
+++ resources/scripts/prepare_wem_files_directory.bat
@@ -1,6 +1,14 @@
 @echo off
-for /R "%~dp0" %%F in (*.english.stream) do ren "%%F" "%%~nF" :: Remove first part of the complex extension
-for /R "%~dp0" %%F in (*.english) do ren "%%F" "%%~nF.wem" :: Rename .english to .wem
-for /R "%~dp0" %%F in (*.stream) do ren "%%F" "%%~nF.wem" :: Rename .stream to .wem
-for /R "%~dp0" %%F in (*.wem) do move /y "%%F" "%~dp0" :: Move them all to folder containing this script
+
+:: Remove first part of the complex extension
+for /R "%~dp0" %%F in (*.english.stream) do ren "%%F" "%%~nF" 
+
+:: Rename .english to .wem
+for /R "%~dp0" %%F in (*.english) do ren "%%F" "%%~nF.wem" 
+
+:: Rename .stream to .wem
+for /R "%~dp0" %%F in (*.stream) do ren "%%F" "%%~nF.wem" 
+
+:: Move them all to folder containing this script
+for /R "%~dp0" %%F in (*.wem) do move /y "%%F" "%~dp0" 
 PAUSE
